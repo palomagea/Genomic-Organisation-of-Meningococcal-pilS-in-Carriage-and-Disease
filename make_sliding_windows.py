a3ef7422208ccab2de62cf003c3d68994a12cc71
palomagea <161571805+palomagea@users.noreply.github.com>
2024-03-06 09:57:21 +1300

    Update make_sliding_windows.py

--- make_sliding_windows.py
+++ make_sliding_windows.py
@@ -7,17 +7,14 @@ import sys
 isolate = sys.argv[1]
 whole_genome_fasta = sys.argv[2]
 query_bed = sys.argv[3] 
-path = "/NGS/active/IPL/MENINGO/analysis/paloma/2023/"
-filename = "_corrected_consensus.fasta"
-deletions_folder = "deletions_across_genome"
+
 
 window_size = 10000
 step_size = 500
 
-bed_file_path = os.path.join(path, deletions_folder, isolate, isolate + "_WG_windows_query.bed")
 
-with open (bed_file_path, 'w') as bed_file:
-    for record in SeqIO.parse(open(os.path.join(path, isolate, isolate + filename)), 'fasta'):
+with open (query_bed, 'w') as bed_file:
+    for record in SeqIO.parse(open(whole_genome_fasta, 'fasta'):
         seq_name = record.id
         seq_length = len(record)
         
