31e3a0f9e5c607b75abc37b2c83668a63b656506
palomagea <161571805+palomagea@users.noreply.github.com>
2024-03-06 14:12:36 +1300

    Update seq_analysis.sh

--- seq_analysis.sh
+++ seq_analysis.sh
@@ -1,4 +1,4 @@
-#This is the first pipeline used to analyse the sequencing run and make a genome assembly from the fastq files
+#This is the first pipeline used to analyse the sequencing run and make a genome assembly from the fastq files, annotate with prokka and give the average depth, read length and longest read
 #This pipeline was used before the issues with medaka were discovered
 
 #!/bin/bash
@@ -9,52 +9,32 @@ isolate="the_name_of_the_isolate_or_experiment" #Multiple isolates were sequence
 seq_run_path="the_name_and_path_where_the_fastq_runs_are_saved" #This is the path to folder of fastq files with the barcode corresponding to this isolate
 seq_summary="the_name_and_path_of_the_sequencing_summary_file_for_this_run" #The path to the sequencing summary file for this run and the name of the summary file
 
-#start in correct folder
-echo Enter pwd for the folder you want to be working in 
-read path_dir
-
-#check which isolate you are analysing and set variable name 
-echo Enter name of run 
-read run 
-
-echo Enter isolate name
-read isolate
-echo Analysing sequence run from isolate $isolate 
-
-
-#make folder for this isolate to store analysis
-mkdir $isolate
-
-#locate path where the sequencing run is stored
-echo Enter path of isolate sequencing run
-read seq_run_path
-
-#make summary file and save in home directory
-cd $seq_run_path
-cd ..
-cd ..
+#Load modules 
 module load pycoqc/2.5.0.3
-pycoQC --summary_file sequencing_summary* -o /home/pgea/${run}_qc.html
- 
-#cat together fastq files from run and move into new directory
-cd $path_dir
-cat $seq_run_path/*.fastq.gz > $path_dir/$isolate/$isolate.fastq.gz
-
-#filter the fastq files to remove any with a q score below 12 or a length below 5kb
 module load nanopack/1.1.1
-cd $isolate
-gunzip $isolate.fastq.gz
-NanoFilt -q 12 --length 5000 $isolate.fastq > ${isolate}_filt.fastq
-
-#run flye to assemble genome and correction step with medaka and racon 
 module load flye/2.8-1
 module load minimap2/2.24
 module load racon/1.4.3
 module load medaka/1.7.1
 
-flye --nano-raw ${isolate}_filt.fastq --threads 20 --iterations 3 --out-dir ${isolate}_flye
-minimap2 -x map-ont -t 20 ${isolate}_flye/assembly.fasta ${isolate}_filt.fastq > ${isolate}_flye/assembly_racon.paf
-racon -m 8 -x -6 -g -8 -w 500 -t 20 ${isolate}_filt.fastq ${isolate}_flye/assembly_racon.paf ${isolate}_flye/assembly.fasta > ${isolate}_flye/racon_on_flye.fasta
+#Make a summary file of the sequencing run
+pycoQC --summary_file seq_summary -o ${run}_qc.html
+
+#Make a directory for the isolate 
+mkdir ${isolate} 
+
+#Join all of the fastq files for the isolate into a single fastq and move into the isolate directory
+cat $seq_run_path/*.fastq.gz > $path_dir/$isolate/$isolate.fastq.gz
+
+#Filter the fastq files to remove any with a q score below 12 or a length below 5kb
+cd ${path_dir}/$isolate
+gunzip $isolate.fastq.gz
+NanoFilt -q 12 --length 5000 $isolate.fastq > ${isolate}_filt.fastq
+
+#Use to assemble genome and polish with Medaka and Racon 
+flye --nano-raw ${isolate}_filt.fastq --threads 20 --iterations 3 --out-dir ${isolate}_flye #Flye is run on the filtered fastq reads and saved in an output directory
+minimap2 -x map-ont -t 20 ${isolate}_flye/assembly.fasta ${isolate}_filt.fastq > ${isolate}_flye/assembly_racon.paf #Minimap is used to map the filtered fastq onto the flye assembled genome and saved in paf format to use with Racon
+racon -m 8 -x -6 -g -8 -w 500 -t 20 ${isolate}_filt.fastq ${isolate}_flye/assembly_racon.paf ${isolate}_flye/assembly.fasta > ${isolate}_flye/racon_on_flye.fasta #Racon is used to 
 medaka_consensus -i ${isolate}_filt.fastq -d ${isolate}_flye/racon_on_flye.fasta -o ${isolate}_flye/medakon_on_racon -t 3 -m r941_min_hac_g507
 
 
