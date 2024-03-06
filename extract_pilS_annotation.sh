497b7023293ad154bb2e419bc7bb50e8c96b7e29
palomagea <161571805+palomagea@users.noreply.github.com>
2024-03-06 15:42:12 +1300

    Update extract_pilS_annotation.sh

--- extract_pilS_annotation.sh
+++ extract_pilS_annotation.sh
@@ -7,6 +7,9 @@
 
 ######### Set the variables #########
 isolate="the_name_of_the_isolate_or_experiment" #Use the same isolate name as in the seq_analysis.sh pipeline so that all of the files are named consistently
+pilS_start="the_genomic_start_positon_of_pilS" #The positon in bp of the start of pilS (including fbp and lpxC +1kb on either end as a buffer)
+pilS_end="the_genomic_end_position_of_pilS" #The positon in bp of the end of pilS (including fbp and lpxC +1kb on either end as a buffer)
+pilS_contig="the_contig_that_pilS_is_on" #Very few of the isolates had more than one contig. If there was only one contig set "contig_1". In isolates with more than one contig set the contig pilS was on. 
 
 #Set the working directory with the scripts saved in it 
 path_dir=$(pwd)
@@ -18,23 +21,19 @@ module load samtools/1.9
 module load bakta/1.5.0
 module load ncbi-blast/2.6.0
 
-#make query.bed for the pilS region from input 
-python make_query.py
+#Run python script to make a query bed file that contains the contig name and start and end positions of pilS
+cd ${isolate}
+python ${path_dir}/make_query.py
 
-#get length of genome and save to txt file 
-awk '/^>/{if (l!="") print l; print; l=0; next}{l+=length($0)}END{print l}' $path_dir/$isolate/${isolate}_corrected_consensus.fasta > $path_dir/$isolate/genome_length.txt 
+#Make a genome file that contains the length of the genome
+awk '/^>/{if (l!="") print l; print; l=0; next}{l+=length($0)}END{print l}' $path_dir/$isolate/${isolate}_corrected_consensus.fasta > $path_dir/$isolate/genome_length.txt #Get the length of genome and save to a txt file
+python ${path_dir}/create_genome.py #Run python script to make a genome file from the txt file with the genome length
 
-#create genome file from input 
-python create_genome.py
+#Extract only the reads that map the whole way across the pilS region
+bedtools intersect -a ${isolate}_allreads.bed -b query.bed -F 1 > ${isolate}_pilS.bed #the query bed file contains the pilS region and the intersect function extracts reads from the allreads.bed file that overlap the whole pilS region
 
-#extract only the reads that map the whole way across the pilS region
-cd $path_dir/$isolate
-bedtools intersect -a ${isolate}_allreads.bed -b query.bed -F 1 > ${isolate}_pilS.bed 
-
-cd $path_dir
-
-#count reads mapping pilS
-python count_pilS_reads.py
+#Run python script that counts reads mapping pilS from the pilS.bed file 
+python ${path_dir}/count_pilS_reads.py
 
 #make pilS fasta
 cd $isolate 
