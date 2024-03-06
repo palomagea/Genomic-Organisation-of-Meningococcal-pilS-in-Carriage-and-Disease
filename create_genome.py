455d7ae09c667f3aa2b581ea762d8ecab36aac52
palomagea <161571805+palomagea@users.noreply.github.com>
2024-03-06 16:00:30 +1300

    Update create_genome.py

--- create_genome.py
+++ create_genome.py
@@ -4,12 +4,11 @@ import sys
 
 #These variables are set in the extract_pilS_annotation.sh pipeline
 genome_length = sys.argv[1]
+output_filename = sys.argv[2] 
 
-with open(genome_length, 'r') as openfile:
+with open(genome_length, 'r') as openfile: #The txt file containing the genome length is opened
     rfile = openfile.readlines()
-    length = rfile[1].strip()  
+    length = rfile[1].strip()  #The length of the genome is saved as the length variable 
 
-output_filename = path + isolate + "/" + isolate + ".genome"
-
-with open(output_filename, 'w') as f:
+with open(output_filename, 'w') as f: #A genome file is made with the length of the genome 
     f.write(f"contig_1\t{length}\n") 
