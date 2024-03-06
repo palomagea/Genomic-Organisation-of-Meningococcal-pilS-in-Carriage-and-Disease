ebdc9418ae1d86ca27697fa1974ecf89c207badb
palomagea <161571805+palomagea@users.noreply.github.com>
2024-03-06 14:45:53 +1300

    Update after_correction.sh

--- after_correction.sh
+++ after_correction.sh
@@ -36,8 +36,8 @@ samtools index ${isolate}_allreads.bam
 samtools depth -aa ${isolate}_allreads.bam > ${isolate}_coverage.txt
 
 #Generate txt files with the lengths of each read mapped to the genome
-samtools view -F 4 ${isolate}_allreads.bam | cut -f 10 | perl -ne 'chomp;print length ($_). "\n"' | sort -n | uniq -c > ${isolate}_numberreads_readlength.txt
-samtools view -F 4 ${isolate}_allreads.bam | awk '{print length($10)}' | sort -n > listreadlengths.txt 
+samtools view -F 4 ${isolate}_allreads.bam | cut -f 10 | perl -ne 'chomp;print length ($_). "\n"' | sort -n | uniq -c > ${isolate}_numberreads_readlength.txt #a txt file is made with two columns, the length of the read (right column) and the number of reads that are that length (left column)
+samtools view -F 4 ${isolate}_allreads.bam | awk '{print length($10)}' | sort -n > listreadlengths.txt #a txt file is generated that has the length of every mapped read on a new line
 
 #run python scripts for analysis need to be in folder with scripts 
 cd $path_dir
