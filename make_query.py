a3958253e92e8cefe6e9e423cf84b52e5af5784a
palomagea <161571805+palomagea@users.noreply.github.com>
2024-03-06 15:51:56 +1300

    Update make_query.py

--- make_query.py
+++ make_query.py
@@ -1,12 +1,14 @@
+#This script works with the extract_pilS_annotation.sh pipeline to create a query file for the pilS region
+#The query file contains the contig pilS is on and the start and end positions of pilS (pilS includes both lpxC and fbp +1kb on either side as a buffer)
 
-isolate = input("input isolate name ")
+import sys 
 
-start_pilS = input(str("input the start of pilS "))
-end_pilS = input(str("input the end of pilS "))
-
-import sys
-
-print ("contig_1\t", start_pilS,"\t", end_pilS)
+#These variables are set in the extract_pilS_annotation.sh pipeline
+isolate = sys.argv[1]
+pilS_start = sys.argv[2]
+pilS_end = sys.argv[3] 
+pilS_contig = sys.argv[4]
+query_bed = sys.argv[5]
 
 original_stdout = sys.stdout
 
