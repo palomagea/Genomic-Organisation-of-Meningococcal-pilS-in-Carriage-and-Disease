8a797776b8db23012da1f758041101466304f350
palomagea <161571805+palomagea@users.noreply.github.com>
2024-03-07 11:46:22 +1300

    Update extract_sequence_name_from_bed.py

--- extract_sequence_name_from_bed.py
+++ extract_sequence_name_from_bed.py
@@ -1,16 +1,15 @@
-isolate = input("input isolate name ")
-path = "/NGS/active/IPL/MENINGO/analysis/paloma/2023/"
-filename = "_pilS.bed"
+#This script works with the pilS_bed.sh pipeline to search the pilS.bed file that contains the reads mapping across pilS and save their sequence names into a pilS_read_names.txt file
 
+import sys 
 
-input_bed_file = path + isolate + "/" + isolate + filename
-output_txt_file = 'pilS_read_names.txt'
+#These variables are set in the extract_pilS_annotation.sh pipeline
+pilS_bed = sys.argv[1]
+pilS_names_txt = sys.argv[2]
 
-with open(input_bed_file, 'r') as bed_file:
+with open(pilS_bed, 'r') as bed_file:
     lines = bed_file.readlines()
 
 sequence_names = [line.split()[3].strip() for line in lines]
 
-with open(output_txt_file, 'w') as txt_file:
+with open(pilS_names_txt, 'w') as txt_file:
     txt_file.write('\n'.join(sequence_names))
-
